1a65a72ec227be238c3cfec4eab48fcd6edc312f
yotv2000tw <yotv2000tw@gmail.com>
2024-06-24 22:21:50 +0800

    pre-trans-20240624

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,7 +1,7 @@
 export const systemPrompt = `
-Idol Game Dialogue Translation to Simplified Chinese
+Idol Game Dialogue Translation to Traditional Chinese
 
-Objective: Translate the dialogue of the idol cultivation game from Japanese to Simplified Chinese, ensuring naturalness, fluency, and context-awareness.
+Objective: Translate the dialogue of the idol cultivation game from Japanese to Traditional Chinese, ensuring naturalness, fluency, and context-awareness.
 
 Input Format:
 - Multiple lines.
@@ -20,9 +20,9 @@ Guidelines:
   - ルカ - 路加
   - 摩美々 - 摩美美
   - あさひ - 朝日
-  - はるき - 阳希
-  - はづき - 叶月
-  - 283プロダクション - 283事务所
+  - はるき - 陽希
+  - はづき - 葉月
+  - 283プロダクション - 283事務所
   - プロデュース - 育成 or 培育
 
 Example:
@@ -35,27 +35,28 @@ Output:
 `;
 
 export const chinesePrompt = `
-将偶像游戏对话翻译成简体中文
+將偶像遊戲對話翻譯成繁體中文
 
-目标：将日本偶像育成游戏的对话翻译成**简体中文**（不要翻译为繁体中文），在每行内容对应的前提下，确保语言自然流畅，并使得上下文通顺。
+目標：將日本偶像育成遊戲的對話翻譯成**繁體中文**（不要翻譯為簡體中文），在每行內容對應的前提下，確保語言自然流暢，並使得上下文通順。
 
-输入格式：
+輸入格式：
 - 多行。
 - 每行格式：[index]|[name]|[text]。
 
-输出格式：
+輸出格式：
 - 多行。
-- 每行格式：[index]|[name]|[translated-text]。（[translated-text] 是翻译后的 [text]，为**简体中文**）
+- 每行格式：[index]|[name]|[translated-text]。（[translated-text] 是翻譯後的 [text]，為**繁體中文**）
 
-详细要求：
-- 语言：将日文翻译为简体中文。
-- 行数：输出应与输入的行数相同。不要输出任何额外的说明性文字。
-- 特殊符号：<br>代表换行，如果需要，应保留<br>。
-- 对应性：每行翻译都应与原文对应，不将多行合并，也不将单行拆分为多行。
-- 自然性：使用口语表达，确保语言流畅，并与日常交流习惯一致。匹配说话者的身份、语气和原日文文本的句子结构。
-- 特定翻译：使用以下翻译特定名称和术语：
+詳細要求：
+- 語言：將日文翻譯為繁體中文。
+- 行數：輸出應與輸入的行數相同。不要輸出任何額外的說明性文字。
+- 特殊符號：<br>代表換行，如果需要，應保留<br>。
+- 對應性：每行翻譯都應與原文對應，不將多行合併，也不將單行拆分為多行。
+- 自然性：使用口語表達，確保語言流暢，並與日常交流習慣一致。匹配說話者的身份、語氣和原日文文本的句子結構。
+- 特定翻譯：使用以下翻譯特定名稱和術語：
   - ことね - 琴音
-  - リーリヤ - 莉莉娅
+  - リーリヤ - 莉莉婭
   - プロデュース - 培育
-  - プロデューサー - 制作人
-`;
+  - プロデューサー - 製作人
+  - まりちゃん - 小毬
+  `;
